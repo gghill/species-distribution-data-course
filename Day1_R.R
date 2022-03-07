a85dd582b07720d15cb208554ed996ef9e589e12
gghill <gghill@alumni.stanford.edu>
2022-03-07 14:32:46 +0100

    init commit species dist R course

---
+++ Day1_R.R
@@ -0,0 +1,4 @@
+# Species occurence data download in R #
+# Offered by transmitting science #
+# 10-11 March 2022 #
+setwd("C:/Users/06108182/OneDrive - Nord universitet/Courses/Downloading sdm data R/species occurence data download R")
