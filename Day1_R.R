35724247609ea2f8e04b773b6480ceca7bf79c22
gghill <gghill@alumni.stanford.edu>
2022-03-08 10:21:31 +0100

    Added setup and required packages

--- Day1_R.R
+++ Day1_R.R
@@ -1,4 +1,15 @@
 # Species occurence data download in R #
 # Offered by transmitting science #
 # 10-11 March 2022 #
+# DAY 1 -------------------------------------------------------------------
+## SETUP ----
 setwd("C:/Users/06108182/OneDrive - Nord universitet/Courses/Downloading sdm data R/species occurence data download R")
+require(spocc)
+require(terra)
+require(raster)
+require(geodata)
+require(leaflet)
+require(neotoma)
+require(data.table)
+
+
---
+++ Day2_R.R
@@ -0,0 +1,12 @@
+# Species occurence data download in R #
+
+# DAY 2 -------------------------------------------------------------------
+
+## SETUP ----
+require(scrubr)
+require(CoordinateCleaner)
+require(spThin)
+require(devtools)
+require(humboldt)
+require(fuzzySim)
+
