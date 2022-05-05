5a629230a763c878e33f23dbb8347ba0d2878275
gghill <gghill@alumni.stanford.edu>
2022-05-05 12:26:06 +0200

    cleanup

--- scripts/Exercise - seaweed eco-regions Norway.R
+++ scripts/Exercise - seaweed eco-regions Norway.R
@@ -56,7 +56,7 @@ crs(shape)
 ## overlaying species occurrence ----
 library(rgbif)
 my_species <- "Saccharina"  # if you then choose a different species, change it here, and not all over the script!
-my_window <- c(6, 11, 64, 79)
+my_window <- c(-1, 64, 11, 79)
 my_window_lon <- paste0(my_window[1:2], collapse = ", ")
 my_window_lat <- paste0(my_window[3:4], collapse = ", ")
 gbif_data <- occ_data(scientificName = my_species, hasCoordinate = TRUE, limit = 5000, decimalLongitude = my_window_lon, decimalLatitude = my_window_lat)
@@ -64,16 +64,18 @@ gbif_occurrences <- gbif_data$data # selecting useful part of large gbif object
 gbif_occurrences
 
 ## try with spocc ----
-spocc_data <- occ(my_species, from = c("gbif", "bison", "inat", "ebird", "vertnet", "idigbio", "obis", "ala"), has_coords = TRUE, limit = 5000)
+spocc_data <- occ(my_species, geometry = my_window, from = c("gbif", "bison", "inat", "ebird", "vertnet", "idigbio", "obis", "ala"), has_coords = TRUE, limit = 5000)
+# this won't run, check geometry documentation again
 # should revise to get more records, but only from target area
+# geometry should do this
 # save this object to disk:
-saveRDS(spocc_data, paste0("./outputs/spocc_", my_species, "_raw.rds"))  # we name it "raw" because the data haven't been cleaned yet
+saveRDS(spocc_data, paste0("./outputs/spocc_", my_species, "restricted_raw.rds"))  # we name it "raw" because the data haven't been cleaned yet
 
-spocc_data <- readRDS(paste0("./outputs/spocc_", my_species, "_raw.rds"))
+spocc_data_restricted <- readRDS(paste0("./outputs/spocc_", my_species, "restricted_raw.rds"))
 
 # combine the 'spocc_data' list into one data frame:
 ?occ2df
-spocc_df <- occ2df(spocc_data)
+spocc_df <- occ2df(spocc_data_restricted)
 spocc_df
 
 str(spocc_df)  # longitude and latitude are character...
@@ -83,14 +85,32 @@ spocc_df$latitude <- as.numeric(spocc_df$latitude)
 
 # poor gbif occurrences
 
+# read in Norwegian aquaculture registry data
+aqua_sites <- read.delim('./data/aqua_sites_algae_exp.txt')
+aqua_sites$label <- with(aqua_sites, paste(
+  Site.Name,
+  "<br>Capacity:", Capacity,
+  "<br>Culture:", Culture
+))
+# doublecheck filtering approach, some sites being excluded
+# such as blue harvest lofoten that have algae
+kelp <- makeIcon('seaweed.png', iconWidth = 18, iconHeight = 18)
+farm <- makeIcon('norway.png', iconWidth = 24, iconHeight = 24)
+html_legend <- "<img src='https://i.ibb.co/4RVNHQW/norway.png'style='width:18px;height:18px;'>Norwegian Algae Farms<br/>
+<img src='https://i.ibb.co/j4S7bB4/seaweed.png'style='width:18px;height:18px;''>Saccharina latissima occurences"
 # map eco regions and species occurrence
 leaflet() %>%
   addTiles() %>%
   flyTo(14.4, 67.28, zoom = 5) %>%
-  addPolygons(data = spTransform(shape,  CRS("+ellps=WGS84 +proj=longlat +datum=WGS84 +no_defs")), 
+  addPolygons(data = spTransform(shape,  CRS("+ellps=WGS84 +proj=longlat +datum=WGS84 +no_defs")),
               col = '#5c8a5a',
               fill = '#c1f2f5') %>%
-  addCircleMarkers(data = spocc_df, lng = ~ longitude, lat = ~ latitude, radius = 5,
-                   clusterOptions = markerClusterOptions())
+  addMarkers(data = spocc_df, lng = ~ longitude, lat = ~ latitude, icon = kelp, label = spocc_df$prov,
+                   clusterOptions = markerClusterOptions()) %>%
+  addMarkers(lng = aqua_sites$Long, lat = aqua_sites$Lat, icon = farm, 
+             label = aqua_sites$Site.Name,
+             popup = paste(aqua_sites$label),
+             clusterOptions = markerClusterOptions()) %>%
+  addControl(html = html_legend, position = 'bottomright')
 
 
